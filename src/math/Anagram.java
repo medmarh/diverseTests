8c9dd7e4f112fe4cdb3e16aadc3d574ae3fc6115
medmarh <medmarh@gmail.com>
2025-11-12 22:51:02 +0100

    anagram strings

--- src/math/Anagram.java
+++ src/math/Anagram.java
@@ -4,36 +4,20 @@ import java.util.Arrays;
 
 public class Anagram {
 
-	private static boolean getProfit(String s, String t) {
+	private static boolean getResult(String s, String t) {
 
 		char[] sChar = s.toCharArray();
 		char[] tChar = t.toCharArray();
-		boolean match = false;
-		
+
 		Arrays.sort(tChar);
 		Arrays.sort(sChar);
-		
-		return Arrays.equals(sChar,tChar);
-//		for(char ss:sChar) {
-//			for(char tt:tChar) {
-//				match = false;
-//				System.out.println(ss +":" + tt);
-//				if(ss==tt) {
-//					match =  true;
-//					break;
-//				}
-//			}
-//			if(!match) {
-//				break;
-//			}
-//		}
-		
-		//return match;
+
+		return Arrays.equals(sChar, tChar);
 	}
 
 	public static void main(String[] args) {
 
-		System.out.println(getProfit("aga", "ag"));
+		System.out.println(getResult("listen", "silent"));
 	}
-	
+
 }
